cb759cbf014ec2ea64e2b5ab9037d24f91ab2cf7
muriloresendesouza-byte <murilo.resende.souza@escola.pr.gov.br>
2026-03-20 11:29:41 -0300

    Add files via upload

---
+++ SQLite (11).sql
@@ -0,0 +1,7 @@
+create table Padaria_pão_quentinho_do_Cellbit(
+id integer primary key autoincrement,
+codigo integer,
+nome text,
+preco float,
+quantidade integer
+);
---
+++ SQLite.1 (4).sql
@@ -0,0 +1,4 @@
+INSERT into Padaria_pão_quentinho_do_Cellbit(codigo,nome,preco,quantidade)
+values (90,'cafe expresso',5.90,'30')
+;
+select * from Padaria_pão_quentinho_do_Cellbit
